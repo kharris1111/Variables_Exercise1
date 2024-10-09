0c1fe05f57602fc5a788e4c6bd6bcc5c6e0ebcc6
Kourtnee Harris <jhtech1111@gmail.com>
2024-10-08 22:29:05 -0400

    Changes made

---
+++ Variables_Exercise1/Program.cs
@@ -0,0 +1,13 @@
+﻿string name = "Ralph";
+int age = 25;
+char middleInitial = 'v'; 
+bool isOver18 = true;
+double currentTemp = 80.9;
+decimal currentPrice = 23.99m;
+
+Console.WriteLine($"Name: {name}");
+Console.WriteLine($"Age: {age}");
+Console.WriteLine($"Middle Initial: {middleInitial}");
+Console.WriteLine($"Is Over 18: {isOver18}");
+Console.WriteLine($"Current Temp: {80.9}");
+Console.WriteLine($"Current Price:{23.99}");
